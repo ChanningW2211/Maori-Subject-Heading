f15015e25aa9ef7332ea39d986ade3977eb6e0ec
ChanningW2211 <60019442+ChanningW2211@users.noreply.github.com>
2022-06-08 00:09:16 +1200

    safekeeping of the progresss

--- web/web/Pages/Index.cshtml.cs
+++ web/web/Pages/Index.cshtml.cs
@@ -2,6 +2,9 @@
 using System.Web;
 using System.Text;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+using System.Text.RegularExpressions;
+
 
 namespace web.Pages;
 
@@ -18,21 +21,31 @@ public class IndexModel : PageModel
 
     public void OnGet() { }
 
-    [BindProperty(SupportsGet=true)]
+    [BindProperty]
     public string? Term { get; set; }
 
     public async Task<IActionResult> OnPostAsync()
     {
         HttpClient client = _httpClientFactory.CreateClient("AllegroGraph");
+
         StringBuilder uri = new StringBuilder();
         uri.Append("test?query=");
-        uri.Append(HttpUtility.UrlEncode(string.Format(
+        string query = string.Format(
             @"SELECT distinct ?s {{
-            optional {{?s skos:prefLabel ""{0}"".}}
-            optional {{?s skos:usedFor ""{0}"". }}
-            optional {{?s skos:altLabel ""{0}"". }}}}", Term)));
+            optional {{ ?s <http://www.w3.org/2008/05/skos#prefLabel> ?o. FILTER (contains(lcase(str(?o)),'{0}')) }}
+            optional {{ ?s <http://www.w3.org/2008/05/skos#usedFor> ?o. FILTER (contains(lcase(str(?o)),'{0}')) }}
+            optional {{ ?s <http://www.w3.org/2008/05/skos#altLabel> ?o. FILTER (contains(lcase(str(?o)),'{0}')) }}}}", Term?.ToLower());
+        uri.Append(HttpUtility.UrlEncode(query));
         HttpResponseMessage response = await client.GetAsync(uri.ToString());
-        string result = response.Content.ReadAsStringAsync().Result;
-        return Page();
+
+        var result = JObject.Parse(response.Content.ReadAsStringAsync().Result)["values"];
+        string pattern = @"""(.*?)""";
+        List<string> terms = new List<string>();
+        foreach (Match match in Regex.Matches(result.ToString(), pattern))
+        {
+            terms.Add(match.Groups[0].Value);
+        }
+        
+        return RedirectToPage("Term", new { term = terms.FirstOrDefault() });
     }
 }
---
+++ web/web/Pages/Term.cshtml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Web;
+using System.Text;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+using System.Text.RegularExpressions;
+
+namespace web
+{
+	public class TermModel : PageModel
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        [ViewData]
+        public Term test { get; set; }
+
+        public class Term
+        {
+            public string PrefLabel { get; set; }
+            public string Tukutuku { get; set; }
+
+            public string AltLabel { get; set; }
+            public string UsedFor { get; set; }
+
+            public string Whakamārama { get; set; }
+            public string ScopeNote { get; set; }
+
+            public List<Uri> RelatedTerm { get; set; }
+            public List<Uri> NarrowerTerm { get; set; }
+            public List<Uri> BroaderTerm { get; set; }
+        }
+
+        public TermModel(IHttpClientFactory httpClientFactory) => _httpClientFactory = httpClientFactory;
+
+        public async Task<ActionResult> OnGetAsync(string term)
+        {
+            HttpClient client = _httpClientFactory.CreateClient("AllegroGraph");
+            StringBuilder uri = new StringBuilder();
+            uri.Append("test?query=");
+            string query = string.Format(@"SELECT ?p ?o {{{0} ?p ?o }}", term.Substring(1,term.Length-2).Replace("%2F", "/"));
+            uri.Append(HttpUtility.UrlEncode(query));
+            HttpResponseMessage response = await client.GetAsync(uri.ToString());
+
+            var result = JObject.Parse(response.Content.ReadAsStringAsync().Result)["values"];
+            string pattern = @"""(.*?)""";
+            List<string> terms = new List<string>();
+            foreach (Match match in Regex.Matches(result.ToString(), pattern))
+            {
+
+                terms.Add(match.Groups[0].Value);
+            }
+
+            return Page();
+        }
+    }
+}
